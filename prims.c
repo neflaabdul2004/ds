4160970be4c277ecc32030c642292d31cb180ed4
neflaabdul2004 <naflarv32@gmail.com>
2025-11-26 12:02:19 +0530

    Enhance comments for clarity in prims.c
    Added comments to clarify code functionality.

--- prims.c
+++ prims.c
@@ -13,14 +13,14 @@ void main() {
         for(j = 1; j <= n; j++) {
             scanf("%d", &cost[i][j]);
             if(cost[i][j] == 0)
-                cost[i][j] = 999;
+                cost[i][j] = 999;//initialize non-exitent edgge with high cost
         }
     }
 
-    visited[1] = 1;
+    visited[1] = 1;//start with node
 
     printf("\n");
-    while(ne < n) {
+    while(ne < n) {//finding minimum spanning tree
         min = 999;
         
         for(i = 1; i <= n; i++)
@@ -31,13 +31,13 @@ void main() {
                 
                     if(visited[i] != 0 && visited[j] == 0) {
                         min = cost[i][j];
-                        u = i;
+                        u = i;//store edge with minimum cost
                         v = j;
                     }
 
         printf("\nEdge %d:(%d %d) cost:%d", ne++, u, v, min);
         mincost += min;
-        visited[v] = 1;
+        visited[v] = 1;//mark v node are visited
         cost[u][v] = cost[v][u] = 999;
     }
 
